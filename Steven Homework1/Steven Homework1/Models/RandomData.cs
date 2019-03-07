32b14fe21774f681c72c04b08ef91d6fe2958435
stevensa123 <steven.chen19@TI03NB69>
2019-03-07 15:48:19 +0800

    change

---
+++ Steven Homework1/Steven Homework1/Models/RandomData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Steven_Homework1.Models
+{
+    public class RandomData
+    {
+        public double money;
+        public static DateTime dt = new DateTime();
+        public String StringDate = string.Format("{0:d}", dt);
+    }
+}
---
+++ Steven Homework1/Steven Homework1/Models/ViewModel/DataPassModel.cs
@@ -0,0 +1,9 @@
+﻿using  Steven_Homework1.Controllers;
+
+namespace Steven_Homework1.Models.ViewModel
+{
+    public class DataPassModel
+    {
+        public RandomData rd { get; set; }
+    }
+}
